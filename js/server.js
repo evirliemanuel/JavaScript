84f27d353836563d1521588646a21180ab35c8dd
inteliedoit <lieverandiver@gmail.com>
2017-11-26 20:40:04 +0800

    Node.JS Create Simple Web File Server

--- js/server.js
+++ js/server.js
@@ -1,12 +1,23 @@
 var http = require('http');
+var fs = require('fs');
 
-function onRequest(request, response){
-	console.log("A user made a request" + request.url);
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.write("Here is some data");
+//404 response
+function send404Response(response){
+	response.writeHead(404, {"Content-Type": "text/plain"});
+	response.write("Error 404: Page not found!");
 	response.end();
 	
+}
+
+//Bandle a user request
+function onRequest(request, response){
 	
+	if(request.method == 'GET' && request.url == '/' ){
+		response.writeHead(200, {"Content-Type": "text/html"});
+		fs.createReadStream("./index.html").pipe(response);
+	}else{
+		send404Response(response);
+	}
 }
 
 http.createServer(onRequest).listen(8080);
