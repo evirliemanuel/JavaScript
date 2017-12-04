71c2cc1e56b26bf650c297542010a4c3f4ef79e9
inteliedoit <lieverandiver@gmail.com>
2017-12-04 21:01:44 +0800

    Express.js route parameter

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ app.get('/profile', function(request, response){
     response.send("This is Profile page");
 });
 
+var students = {
+    1 : 'Rafael',
+    name : 'Ronald' ,
+    age : 16
+}
+app.get('/student/:id', function(request, response){
+    response.send("you have request to see student name :" + students[request.params.id] );
+});
 app.listen(3000, function(){
     console.log("Server is now running...");
 });
