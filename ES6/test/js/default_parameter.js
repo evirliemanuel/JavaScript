0b3431bf1716b8f763b566f85d8df1e82833739f
inteliedoit <lieverandiver@gmail.com>
2017-12-28 20:37:18 +0800

    Add some project

--- ES6/test/js/default_parameter.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-
-  function setName(name = "Verlie", age=19) {
-    console.log(`My name is $[{name} and my age ${age}`);
-  }
-  setName();
-
-}
--- ES6/test/js/string_template.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-
-function setName(name, age) {
-  console.log(`My name is ${name} and my age is ${age}`);
-}
-setName("Virlie", 19);
-
-
-}
