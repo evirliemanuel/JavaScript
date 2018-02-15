65232c639c314ee6ab9c367c03e8f1452315fc4e
inteliedoit <lieverandiver@gmail.com>
2018-02-15 21:41:41 +0800

    --

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
-app.listen(4000, function(){
-	console.log("server is now running...");
+
+app.listen(3000, function(){
+	console.log("Server is now running...");
 });
